da7e8d6707912cc87e71e39062212a247f870145
alvarovillena <alvarovillenamg@gmail.com>
2016-04-05 13:48:42 +0200

    Add fixed tf transforms broadcaster

---
+++ src/tf_broadcaster.cpp
@@ -0,0 +1,36 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+
+int main(int argc, char** argv){
+  ros::init(argc, argv, "tf_publisher");
+  ros::NodeHandle n;
+
+  ros::Rate r(100);
+
+  tf::TransformBroadcaster broadcaster;
+
+  while(n.ok()){
+  	
+  	broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.0)),
+        ros::Time::now(),"world", "home"));
+  
+    broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.021, 0.0, 0.33)),
+        ros::Time::now(),"base", "laser"));
+    
+    broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 1.17)),
+        ros::Time::now(),"base", "rgbd"));
+         
+    broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.92)),
+        ros::Time::now(),"world", "velodyne"));
+        
+    r.sleep();
+  }
+}
