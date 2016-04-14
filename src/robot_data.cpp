c8d7cd50809c1ee5a63ca35a511be2980bd69605
alvarovillena <alvarovillenamg@gmail.com>
2016-04-14 10:27:49 +0200

    Add robot position to TF (without rotation)

--- src/robot_data.cpp
+++ src/robot_data.cpp
@@ -1,21 +1,34 @@
 #include "ros/ros.h"
 #include <Ice/Ice.h>
 #include "../include/AriaMapInformation.h"
-//#include <tf/transform_listener.h>
+#include "geometry_msgs/PointStamped.h"
+#include <tf/transform_broadcaster.h>
 
 using namespace RoboCompAriaMapInformation;
 using namespace std;
+
+geometry_msgs::PointStamped robotPose;
 
 class AriaMapInformationI : public AriaMapInformation {
 public:
 	virtual void robotInformation(const mapPose& m, const Ice::Current&);
 	virtual void mapInformation(const MapInfo& mi, const Ice::Current&);
 	virtual void goalInformation(const GoalInfo& g, const Ice::Current&);
+	tf::TransformBroadcaster broadcaster;
 };
 
 void AriaMapInformationI::robotInformation(const mapPose& m, const Ice::Current&)
 {
-    cout << "Robot: " << m.X << ", " << m.Y << endl;
+    robotPose.point.x = m.X / 1000.0;
+    robotPose.point.y = m.Y / 1000.0;
+    cout << "Robot: " << robotPose.point.x << ", " << robotPose.point.y << endl;
+    cout << "Robot in ARIA coords: " << m.X << ", " << m.Y << endl;
+    
+    //TODO: Not tracking rotation right now. Change idsl definition to include Th field
+    broadcaster.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(robotPose.point.x, robotPose.point.y, 0.0)),
+        ros::Time::now(),"world", "base"));
 }
 
 void AriaMapInformationI::mapInformation(const MapInfo& mi, const Ice::Current&)
@@ -28,30 +41,22 @@ void AriaMapInformationI::goalInformation(const GoalInfo& g, const Ice::Current&
     cout << " " << endl;
 }
 
+
 int main(int argc, char** argv){
   ros::init(argc, argv, "robot_data");
   ros::NodeHandle n;
   Ice::CommunicatorPtr ic;
-
   ros::Rate r(100);
+  ros::Publisher pub = n.advertise<geometry_msgs::PointStamped> ("robot_position", 1);
+  
 	ic = Ice::initialize(argc, argv);
-
-	try {
-      
+	try {      
       Ice::ObjectAdapterPtr adapter =
           ic->createObjectAdapterWithEndpoints("ariamapinformation", "tcp -p 19876");
       Ice::ObjectPtr object = new AriaMapInformationI;
-      //i
       Ice::ObjectPrx mapinformation = adapter->addWithUUID(object)->ice_oneway();
-      //
       adapter->add(object, ic->stringToIdentity("AriaMapInformation.Endpoints"));
-      //i
-      cout << "rodaballo" << endl;
-      //
-      
       adapter->activate();
-
-      //ic->waitForShutdown();
     } catch (const Ice::Exception& e) {
       cerr << e << endl;
     } catch (const char* msg) {
@@ -59,6 +64,10 @@ int main(int argc, char** argv){
     }
 
   while(n.ok()){   
+  	robotPose.header.frame_id = "velodyne";
+		robotPose.header.stamp = ros::Time();
+		pub.publish(robotPose);
+  	
   	r.sleep();
   }
 }
